6083e0219910040b35997ef9d7ebb86c164dc4b1
Warren Harper <warrenjharper@gmail.com>
2015-07-26 12:14:40 -0400

    Add test database

--- env/provision-env.sh
+++ env/provision-env.sh
@@ -10,3 +10,5 @@ sudo -u postgres psql -c "DROP USER $PGUSER" || 0
 sudo -u postgres psql -c "CREATE USER $PGUSER WITH password '$PGPASSWORD'"
 sudo -u postgres psql -c "CREATE DATABASE $DATABASE"
 psql -U $PGUSER -d $DATABASE -a -f ${DIR}/create_db.sql -h localhost
+sudo -u postgres psql -c "DROP DATABASE apitest" || 0
+sudo -u postgres psql -c "CREATE DATABASE apitest WITH TEMPLATE $DATABASE"
