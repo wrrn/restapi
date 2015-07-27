a677bbeac89cf47799dfe2c048b0112c978d321c
Warren Harper <warrenjharper@gmail.com>
2015-07-27 12:59:48 -0400

    Get URL variables

--- utils/request/request.go
+++ utils/request/request.go
@@ -5,6 +5,16 @@ import (
 	"strings"
 )
 
+// Is does a case insensitive check that the request's method matches
+// the method argument
 func Is(r *http.Request, method string) bool {
 	return strings.ToUpper(r.Method) == strings.ToUpper(method)
+}
+
+// GetUrlVariables returns a list variable from a path delimited by the forward
+// slash ("/") character
+func GetURLVariables(path string) []string {
+	path = strings.Trim(path, "/")
+	variables := strings.Split(path, "/")
+	return variables
 }
