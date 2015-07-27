6f8b4b7c258994a9c041516273bccf518af6caaa
Warren Harper <warrenjharper@gmail.com>
2015-07-27 16:41:47 -0400

    Add sorting

--- configuration/configs.go
+++ configuration/configs.go
@@ -1,15 +1,30 @@
 package configuration
 
+import "sort"
+
+type Orderer func(i, j int) bool
+
 type Configurations struct {
 	Configs []Configuration `json:"configurations"`
 }
 
+// GetFirst retrieves the first configuration from the Configurations object.
+// If the length Configurations object's Configs is less than 1 it returns an
+// uninitialized struct.
 func (cs Configurations) GetFirst() (config Configuration) {
 	if len(cs.Configs) > 0 {
 		return cs.Configs[0]
 	}
 	return config
 }
+
+// Sorter sorts functions based on it's Order
+type Sorter struct {
+	Configs []Configuration
+	Order   Orderer
+}
+
+// Equals determines if two Configuration slices are equal
 func Equals(x, y []Configuration) bool {
 	if len(x) != len(y) {
 		return false
@@ -18,8 +33,6 @@ func Equals(x, y []Configuration) bool {
 	for _, configX := range x {
 		var found bool
 		for _, configY := range y {
-
-			// This is so that we don't have to worry about database assigned ids
 
 			if EqualConfigurations(configX, configY) {
 				found = true
@@ -32,4 +45,56 @@ func Equals(x, y []Configuration) bool {
 		}
 	}
 	return true
+}
+
+// Sort sorts the configs according to the order that was given.
+func (s *Sorter) Sort(o Orderer, configs []Configuration) []Configuration {
+	s.Order = o
+	sort.Sort(s)
+	return s.Configs
+}
+
+// Sort reverse sorts the configs according to the order that was given.
+func (s *Sorter) Reverse(o Orderer, configs []Configuration) []Configuration {
+	s.Order = o
+	sort.Reverse(s)
+	return s.Configs
+}
+
+func (s Sorter) Len() int {
+	return len(s.Configs)
+}
+
+func (s Sorter) Swap(i, j int) {
+	temp := s.Configs[i]
+	s.Configs[i] = s.Configs[j]
+	s.Configs[j] = temp
+}
+
+func (s Sorter) Less(i, j int) bool {
+	return s.Order(i, j)
+}
+
+// ByName returns true if the name of the configuration at index i is less than
+// the name of the configuration at index j.
+func (s Sorter) ByName(i, j int) bool {
+	return s.Configs[i].Name < s.Configs[j].Name
+}
+
+// ByHostName returns true if the HostName of the configuration at index i is less than
+// the HostName of the configuration at index j.
+func (s Sorter) ByHostName(i, j int) bool {
+	return s.Configs[i].HostName < s.Configs[j].HostName
+}
+
+// ByPort returns true if the port of the configuration at index i is less than
+// the port of the configuration at index j.
+func (s Sorter) ByPort(i, j int) bool {
+	return s.Configs[i].Port < s.Configs[j].Port
+}
+
+// ByUsername returns true if the username of the configuration at index i is less than
+// the username of the configuration at index j.
+func (s Sorter) ByUsername(i, j int) bool {
+	return s.Configs[i].Username < s.Configs[i].Username
 }
